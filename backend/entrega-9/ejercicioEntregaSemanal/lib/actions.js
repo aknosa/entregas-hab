55ae250abe5848b18db0e02507ea8836ffb80cae
andres <andres.canosa.quintero@gmail.com>
2020-06-14 19:56:48 +0200

    Entrega semanal del módulo de Backend: Ejercicio To Do List

---
+++ backend/entrega-9/ejercicioEntregaSemanal/lib/actions.js
@@ -0,0 +1,112 @@
+"use strict";
+
+const fs = require("fs").promises;
+const path = require("path");
+const chalk = require("chalk");
+const os = require("os");
+const { formatDistanceToNow } = require("date-fns");
+const { es } = require("date-fns/locale");
+
+const todoFile = path.join(os.homedir(), ".tasks.json");
+
+// Lee la lista de todos y devuelve el contenido
+async function readTodoList() {
+  try {
+    let todos;
+
+    try {
+      const todoListContent = await fs.readFile(todoFile);
+      todos = JSON.parse(todoListContent.toString());
+    } catch (error) {
+      todos = { todos: [] };
+      await fs.writeFile(todoFile, JSON.stringify(todos));
+    }
+
+    return todos;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Guarda la lista de todos
+async function saveTodos(todoList) {
+  try {
+    await fs.writeFile(todoFile, JSON.stringify(todoList));
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+async function addTodo({ text, priority, done, date }) {
+  // Abrir la lista actual de todos
+  const currentList = await readTodoList();
+  // Añadir el todo que recibe al principio
+  currentList.todos.unshift({
+    text,
+    priority,
+    done,
+    date,
+  });
+  // Guardar la lista actualizada
+  await saveTodos(currentList);
+}
+
+async function markAsDone(index) {
+  // Abrir la lista actual de todos
+  const todos = await readTodoList();
+  const todosArray = todos.todos;
+  let newIndex = index - 1;
+  // Buscar el todo con el index que recibe
+  // Modificar el objecto del todo como done: true
+  todos.todos[newIndex].done = true;
+  // Guardar la lista actualizada
+  await saveTodos(todos);
+}
+
+async function markAsUnDone(index) {
+  // Abrir la lista actual de todos
+  const todos = await readTodoList();
+  const todosArray = todos.todos;
+  // Buscar el todo con el index que recibe
+  let newIndex = index - 1;
+  // Modificar el objecto del todo como done: false
+  todos.todos[newIndex].done = false;
+  // Guardar la lista actualizada
+  await saveTodos(todos);
+}
+
+async function listTodos() {
+  // Abrir la lista actual de todos
+  let todos = await readTodoList();
+  let todosArray = todos.todos;
+  // Imprimir la lista en la consola
+  for (let todo of todosArray) {
+    let todoDate = formatDistanceToNow(new Date(todo.date), { locale: es });
+    console.log(
+      `${todosArray.indexOf(todo) + 1}. ${todo.text}${
+        todo.priority ? chalk.red(" (Prioridad alta)") : ""
+      } - ${
+        todo.done ? chalk.green("(Hecho)") : "(Pendiente)"
+      } - Añadido hace ${todoDate}.`
+    );
+    console.log();
+  }
+}
+
+async function cleanTodos() {
+  // Abrir la lista actual de todos
+  const todos = await readTodoList();
+  // Filtrar los que están marcados como done: true
+  const filteredTodos = todos["todos"].filter((todo) => todo.done === false);
+  todos.todos = filteredTodos;
+  // Guardar la lista resultante
+  await saveTodos(todos);
+}
+
+module.exports = {
+  addTodo,
+  markAsDone,
+  markAsUnDone,
+  listTodos,
+  cleanTodos,
+};
---
+++ backend/entrega-9/ejercicioEntregaSemanal/todo.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const fs = require("fs").promises;
+const path = require("path");
+const minimist = require("minimist");
+const {
+  addTodo,
+  markAsDone,
+  markAsUnDone,
+  listTodos,
+  cleanTodos,
+} = require("./lib/actions");
+
+async function todoList() {
+  const args = minimist(process.argv.slice(2));
+
+  const { _, priority, list, done, undone, clean } = args;
+
+  if (list) {
+    await listTodos();
+    process.exit();
+  }
+
+  if (done) {
+    // Marcar el todo número "done" como hecho
+    await markAsDone(done);
+    console.log(`Marco el todo ${done} como HECHO`);
+    process.exit();
+  }
+
+  if (undone) {
+    // Marcar el todo número "undone" como no hecho
+    await markAsUnDone(undone);
+    console.log(`Marco el todo ${undone} como PENDIENTE`);
+    process.exit();
+  }
+
+  if (clean) {
+    // Borro de la lista todos hechos
+    await cleanTodos();
+    console.log("Borro los todos ya hechos");
+    process.exit();
+  }
+
+  const newTodo = _.join(" ");
+
+  if (newTodo.length > 0) {
+    // Añadir newTodo a la lista
+    await addTodo({
+      text: newTodo,
+      priority: priority ? true : false,
+      done: false,
+      date: new Date(),
+    });
+
+    process.exit();
+  } else {
+    console.log("Nada que hacer");
+  }
+}
+
+todoList();
